7766ccd32f9293b493afa5db2e7b1a50ccf7111a
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2021-08-29 04:25:14 +0700

    add plugin vuetify

---
+++ todo-ninja/src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <v-app>
+    <v-content>
+      <rooter>
+
+
+      </rooter>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
